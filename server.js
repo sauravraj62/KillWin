75e0b525a8ad21f64bbc5feb8f66d0ef0050e5bf
Saurav Raj <sauravraj62@gmail.com>
2020-10-21 08:40:06 +0530

    Added html response from server

--- server.js
+++ server.js
@@ -11,6 +11,15 @@ const http = require('http'); // import http module
 
 const server = http.createServer((req, res) => {
     console.log('Server pinged');
+    console.log(req.url, req.method, req.headers);
+    res.setHeader('Content-Type', 'text/html'); // make response type as html
+    // creating html response
+    res.write('<html>');
+    res.write('<head><title>Saurav Raj</title></head>');
+    res.write('<body><h1>This is my First response from server.</h1></body>');
+    res.write('</html>');
+    res.end();
+    //process.exit(); // it stops the running server
 });
 
 server.listen(3000);
